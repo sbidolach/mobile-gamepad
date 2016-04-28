b0705882e9c102b55848edf54695723c810542d5
Seweryn Bidolach <sbidolach@op.pl>
2016-04-28 21:24:58 +0100

    send button event from client to server

--- server/src/gamepad.js
+++ server/src/gamepad.js
@@ -1,82 +1,117 @@
 var config = require('../../config/config');
 var uinput = require('uinput');
+var fs = require('fs');
+var ioctl = require('ioctl');
+var struct = require('struct');
 
 var gamepad = function(inputId){
 
   this.inputId = inputId;
 
-  this.setup_options = {
-    EV_KEY : [
-      uinput.KEY_UP,
-      uinput.KEY_LEFT,
-      uinput.KEY_RIGHT,
-      uinput.KEY_DOWN
-    ]
-  };
-
   return {
     disconnect: function(){
 
-      this.stream = undefined;
-      return null;
-
-    }.bind(this),
-    connect: function(){
-
-      uinput.setup(this.setup_options, function(err, stream) {
-        if (err) {throw(err);}
-
-        var create_options = {
-          name : 'MobileGamePad',
-          id : {
-            bustype : uinput.BUS_USB,
-            vendor : 0x3,
-            product : 0x3,
-            version : 2
-          }
-        };
-
-        uinput.create(stream, create_options, function(err) {
-          if (err) {
-              throw(err);
-          }
-
-          this.stream = stream;
-
-        }.bind(this));
-
-      }.bind(this));
-
-    }.bind(this),
-    sendEvent: function(event){
-
-      var code;
-      switch (event) {
-        case 'left':
-          code = uinput.KEY_LEFT;
-          break;
-        case 'right':
-          code = uinput.KEY_RIGHT;
-          break;
-        case 'up':
-          code = uinput.KEY_UP;
-          break;
-        case 'down':
-          code = uinput.KEY_DOWN;
-          break;
-        default:
-          code = 'ERROR';
+      if (this.fd) {
+        ioctl(this.fd, uinput.UI_DEV_DESTROY);
+        fs.close(this.fd);
+        this.fd = undefined;
       }
 
-      uinput.key_event(this.stream, code, function(err) {
-          if (err) {
-              throw(err);
+      return null;
+    }.bind(this),
+
+    connect: function(){
+
+      console.log('connect gamepad');
+
+      fs.open('/dev/uinput', 'w+',
+        function(err, fd) {
+          if (err){
+            console.log(err);
+            throw(err);
           }
-      });
 
+          this.fd = fd;
+
+          ioctl(this.fd, uinput.UI_SET_EVBIT, uinput.EV_KEY);
+          ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_A);
+          ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_B);
+          ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_X);
+          ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_Y);
+          ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_TL);
+          ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_TR);
+          ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_START);
+          ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_SELECT);
+          ioctl(this.fd, uinput.UI_SET_EVBIT, uinput.EV_ABS);
+          ioctl(this.fd, uinput.UI_SET_ABSBIT, uinput.ABS_X);
+          ioctl(this.fd, uinput.UI_SET_ABSBIT, uinput.ABS_Y);
+
+          var name    = "MobileGamePad"; // max 80 chars
+          var bustype = uinput.BUS_USB; // USB
+          var vendor  = 0x5432; // Just some random numbers
+          var product = 0x1515;
+          var version = 1;
+
+          var buffer = new Buffer(1116);
+          buffer.fill(0);
+          buffer.write(name, 0, 80);
+          buffer.writeUInt16LE(bustype, 80 );
+          buffer.writeUInt16LE(vendor , 82 );
+          buffer.writeUInt16LE(product, 84 );
+          buffer.writeUInt16LE(version, 86 );
+
+          fs.write(fd, buffer, 0, buffer.length, null, function(err, written, buffer) {
+            if (err) {
+              console.log(err);
+              throw(err);
+            } else {
+              ioctl(this.fd, uinput.UI_DEV_CREATE);
+            }
+          }.bind(this));
+
+        }.bind(this)
+      );
+
+    }.bind(this),
+
+    sendEvent: function(event){
+
+      if (this.fd) {
+
+        var ev = new Buffer(24);
+        ev.fill(0);
+
+        var tv_sec   = Math.round( Date.now() / 1000 ),
+            tv_usec  = Math.round( Date.now() % 1000 * 1000 ),
+            type     = event.type // uinput.EV_KEY
+            code     = event.code, // ABS_X,
+            value    = event.value; // 1
+
+        ev.writeInt32LE(tv_sec , 0);
+        ev.writeInt32LE(tv_usec, 8);
+        ev.writeInt16LE(type   , 16);
+        ev.writeInt16LE(code   , 18);
+        ev.writeInt32LE(value  , 20);
+
+        console.log('type: ' + type);
+        console.log('code: ' + code);
+        console.log('value: ' + value);
+        console.log('buffer: ' + ev);
+        console.log('###############');
+
+        var ev_end = new Buffer(24);
+        ev_end.fill(0);
+
+        ev_end.writeInt32LE(tv_sec , 0);
+        ev_end.writeInt32LE(tv_usec, 8);
+
+        fs.writeSync(this.fd, ev, 0, ev.length, null);
+        fs.writeSync(this.fd, ev_end, 0, ev_end.length, null);
+
+        return null;
+      }
     }.bind(this)
-  };
-
+  }
 };
 
 module.exports = gamepad;
