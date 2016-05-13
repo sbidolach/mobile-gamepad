fb063faad26e3a2830c1e80b20ec54705aaee84d
Seweryn Bidolach <sbidolach@op.pl>
2016-05-13 23:42:30 +0100

    joystick configuration

--- client/js/client.js
+++ client/js/client.js
@@ -1,54 +1,113 @@
-
 $(window).load(function() {
 
-    // turn off context menu
-    document.oncontextmenu = function(event){
-      if(event.preventDefault){ event.preventDefault(); }
-      if(event.stopPropagation){ event.stopPropagation(); }
-      event.cancelBubble = true;
-      return false;
+  // turn off context menu
+  document.oncontextmenu = function(event) {
+    if (event.preventDefault) {
+      event.preventDefault();
     }
+    if (event.stopPropagation) {
+      event.stopPropagation();
+    }
+    event.cancelBubble = true;
+    return false;
+  }
 
-    // create socket connection
-    var socket = io();
-    socket
-      .on("connect", function() {
-          socket.emit('hello', 'add new input');
-      })
-      .on("hello", function(data) {
-        var gamePadId = data.inputId;
-        console.log(gamePadId);
+  // create socket connection
+  var socket = io();
+  socket
+    .on("connect", function() {
+      socket.emit('hello', 'add new input');
+    })
+    .on("hello", function(data) {
+      var gamePadId = data.inputId;
 
-        $(".btn")
-          .off("touchstart touchend")
-          .on("touchstart", function(event) {
-            socket.emit("event", {type: 0x01, code: $(this).data("code"), value: 1});
-            $(this).addClass("active");
-            // hapticCallback();
-          })
-          .on("touchend", function(event) {
-            socket.emit("event", {type: 0x01, code: $(this).data("code"), value: 0});
-            $(this).removeClass("active");
+      $(".btn")
+        .off("touchstart touchend")
+        .on("touchstart", function(event) {
+          socket.emit("event", {
+            type: 0x01,
+            code: $(this).data("code"),
+            value: 1
           });
-      });
+          $(this).addClass("active");
+          // hapticCallback();
+        })
+        .on("touchend", function(event) {
+          socket.emit("event", {
+            type: 0x01,
+            code: $(this).data("code"),
+            value: 0
+          });
+          $(this).removeClass("active");
+        });
+    });
 
-    // Create Joystick
-    nipplejs.create({
-      zone: document.querySelector('.joystick'),
-      mode: 'static',
-      color: 'black',
-      position: {left: '50%', top: '50%'},
-      multitouch: true
-    // })
-    // .on('start end', function(evt, data) {
-    // })
-    // .on('move', function(evt, data) {
-    }).on('dir:up plain:up dir:left plain:left dir:down ' +
-      'plain:down dir:right plain:right', function(evt, data) {
-        socket.emit("event", {type: 0x03, code: 0x00, value: data.position.x});
-        socket.emit("event", {type: 0x03, code: 0x01, value: data.position.y});
-    // })
-    // .on('pressure', function(evt, data) {
+  setEvent = function(type, code, value) {
+    socket.emit("event", {
+      type: type,
+      code: code,
+      value: value
+    });
+  }
+
+  setDirection = function(event, data) {
+    switch (event.type) {
+      case "dir:left":
+        setEvent(0x01, 0x222, 1);
+        setEvent(0x01, 0x222, 0);
+        break;
+      case "dir:right":
+        setEvent(0x01, 0x223, 1);
+        setEvent(0x01, 0x223, 0);
+        break;
+      case "dir:up":
+        setEvent(0x01, 0x220, 1);
+        setEvent(0x01, 0x220, 0);
+        break;
+      case "dir:down":
+        setEvent(0x01, 0x221, 1);
+        setEvent(0x01, 0x221, 0);
+        break;
+    }
+  };
+
+  var prevType;
+
+  // Create Joystick
+  nipplejs.create({
+    zone: document.querySelector('.joystick'),
+    mode: 'static',
+    color: 'black',
+    position: {
+      left: '50%',
+      top: '50%'
+    },
+    multitouch: true
+      // })
+      // .on('start end', function(evt, data) {
+      // })
+      // .on('move', function(evt, data) {
+      // }).on('dir:up plain:up dir:left plain:left dir:down ' +
+      //   'plain:down dir:right plain:right',
+  }).on('dir:up dir:left dir:down dir:right ',
+    function(evt, data) {
+
+      // if(evt.type !== prevType) {
+      console.log(evt.type);
+      setDirection(evt, data);
+      // prevType = evt.type;
+      // }
+
+      // else {
+      //     setEvent(0x03, 0x00, 127);
+      //     setEvent(0x03, 0x01, 127);
+      // }
+
+      // socket.emit("event", {type: 0x03, code: 0x00, value: data.position.x});
+      // socket.emit("event", {type: 0x03, code: 0x01, value: data.position.y});
+
+      // })
+      // .on('pressure', function(evt, data) {
     });
 
 });
--- server/src/gamepad.js
+++ server/src/gamepad.js
@@ -4,12 +4,12 @@ var fs = require('fs');
 var ioctl = require('ioctl');
 var struct = require('struct');
 
-var gamepad = function(inputId){
+var gamepad = function(inputId) {
 
   this.inputId = inputId;
 
   return {
-    disconnect: function(){
+    disconnect: function() {
 
       if (this.fd) {
         ioctl(this.fd, uinput.UI_DEV_DESTROY);
@@ -20,15 +20,15 @@ var gamepad = function(inputId){
       return null;
     }.bind(this),
 
-    connect: function(){
+    connect: function() {
 
       console.log('connect gamepad');
 
       fs.open('/dev/uinput', 'w+',
         function(err, fd) {
-          if (err){
+          if (err) {
             console.log(err);
-            throw(err);
+            throw (err);
           }
 
           this.fd = fd;
@@ -42,28 +42,34 @@ var gamepad = function(inputId){
           ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_TR);
           ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_START);
           ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_SELECT);
+
+          ioctl(this.fd, uinput.UI_SET_KEYBIT, 0x220);
+          ioctl(this.fd, uinput.UI_SET_KEYBIT, 0x221);
+          ioctl(this.fd, uinput.UI_SET_KEYBIT, 0x222);
+          ioctl(this.fd, uinput.UI_SET_KEYBIT, 0x223);
+
           ioctl(this.fd, uinput.UI_SET_EVBIT, uinput.EV_ABS);
           ioctl(this.fd, uinput.UI_SET_ABSBIT, uinput.ABS_X);
           ioctl(this.fd, uinput.UI_SET_ABSBIT, uinput.ABS_Y);
 
-          var name    = "MobileGamePad"; // max 80 chars
+          var name = "MobileGamePad"; // max 80 chars
           var bustype = uinput.BUS_USB; // USB
-          var vendor  = 0x5432; // Just some random numbers
+          var vendor = 0x5432; // Just some random numbers
           var product = 0x1515;
           var version = 1;
 
           var buffer = new Buffer(1116);
           buffer.fill(0);
           buffer.write(name, 0, 80);
-          buffer.writeUInt16LE(bustype, 80 );
-          buffer.writeUInt16LE(vendor , 82 );
-          buffer.writeUInt16LE(product, 84 );
-          buffer.writeUInt16LE(version, 86 );
+          buffer.writeUInt16LE(bustype, 80);
+          buffer.writeUInt16LE(vendor, 82);
+          buffer.writeUInt16LE(product, 84);
+          buffer.writeUInt16LE(version, 86);
 
           fs.write(fd, buffer, 0, buffer.length, null, function(err, written, buffer) {
             if (err) {
               console.log(err);
-              throw(err);
+              throw (err);
             } else {
               ioctl(this.fd, uinput.UI_DEV_CREATE);
             }
@@ -74,24 +80,24 @@ var gamepad = function(inputId){
 
     }.bind(this),
 
-    sendEvent: function(event){
+    sendEvent: function(event) {
 
       if (this.fd) {
 
         var ev = new Buffer(24);
         ev.fill(0);
 
-        var tv_sec   = Math.round( Date.now() / 1000 ),
-            tv_usec  = Math.round( Date.now() % 1000 * 1000 ),
-            type     = event.type // uinput.EV_KEY
-            code     = event.code, // ABS_X,
-            value    = event.value; // 1
+        var tv_sec = Math.round(Date.now() / 1000),
+        tv_usec = Math.round(Date.now() % 1000 * 1000),
+        type = event.type, // uinput.EV_KEY
+        code = event.code, // ABS_X,
+        value = event.value; // 1
 
-        ev.writeInt32LE(tv_sec , 0);
+        ev.writeInt32LE(tv_sec, 0);
         ev.writeInt32LE(tv_usec, 8);
-        ev.writeInt16LE(type   , 16);
-        ev.writeInt16LE(code   , 18);
-        ev.writeInt32LE(value  , 20);
+        ev.writeInt16LE(type, 16);
+        ev.writeInt16LE(code, 18);
+        ev.writeInt32LE(value, 20);
 
         console.log('type: ' + type);
         console.log('code: ' + code);
@@ -102,7 +108,7 @@ var gamepad = function(inputId){
         var ev_end = new Buffer(24);
         ev_end.fill(0);
 
-        ev_end.writeInt32LE(tv_sec , 0);
+        ev_end.writeInt32LE(tv_sec, 0);
         ev_end.writeInt32LE(tv_usec, 8);
 
         fs.writeSync(this.fd, ev, 0, ev.length, null);
