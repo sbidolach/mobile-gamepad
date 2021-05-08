9be07ee8a1940e4a30b5c260aa3a6b5226220087
Seweryn Bidolach <sbidolach@op.pl>
2021-05-08 15:54:05 +0100

    update fs

--- server/src/gamepad.js
+++ server/src/gamepad.js
@@ -4,12 +4,14 @@ var fs = require('fs');
 var ioctl = require('ioctl');
 var Struct = require('struct');
 
-var gamepad = function(inputId) {
+var gamepad = (inputId) => {
 
     this.inputId = inputId;
 
     return {
-        disconnect: function() {
+        disconnect: () => {
+
+            console.log(`disconnect this.fd = ${this.fd}`)
 
             if (this.fd) {
                 ioctl(this.fd, uinput.UI_DEV_DESTROY);
@@ -18,83 +20,83 @@ var gamepad = function(inputId) {
             }
 
             return null;
-        }.bind(this),
+        },
 
-        connect: function() {
+        connect: () => {
 
             console.log('connect gamepad');
 
-            fs.open('/dev/uinput', 'w+',
-                function(err, fd) {
+            fs.open('/dev/uinput', 'w+', (err, fd) => {
+                if (err) {
+                    console.log(err);
+                    throw (err);
+                }
+
+                this.fd = fd;
+
+                console.log(`connect this.fd = ${this.fd}`)
+
+                ioctl(this.fd, uinput.UI_SET_EVBIT, uinput.EV_KEY);
+                ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_A);
+                ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_B);
+                ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_X);
+                ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_Y);
+                ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_TL);
+                ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_TR);
+                ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_TL2);
+                ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_TR2);
+                ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_START);
+                ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_SELECT);
+                ioctl(this.fd, uinput.UI_SET_EVBIT, uinput.EV_ABS);
+                ioctl(this.fd, uinput.UI_SET_ABSBIT, uinput.ABS_X);
+                ioctl(this.fd, uinput.UI_SET_ABSBIT, uinput.ABS_Y);
+
+                var input_id = Struct()
+                    .word16Ule('bustype')
+                    .word16Ule('vendor')
+                    .word16Ule('product')
+                    .word16Ule('version');
+
+                var uinput_user_dev = Struct()
+                    .chars('name', uinput.UINPUT_MAX_NAME_SIZE)
+                    .struct('id', input_id)
+                    .word32Ule('ff_effects_max')
+                    .array('absmax', uinput.ABS_CNT, 'word32Sle')
+                    .array('absmin', uinput.ABS_CNT, 'word32Sle')
+                    .array('absfuzz', uinput.ABS_CNT, 'word32Sle')
+                    .array('absflat', uinput.ABS_CNT, 'word32Sle');
+
+                uinput_user_dev.allocate();
+                var buffer = uinput_user_dev.buffer();
+                var uidev = uinput_user_dev.fields;
+                buffer.fill(0);
+
+                uidev.name = "MobileGamePad";
+                uidev.id.bustype = uinput.BUS_USB;
+                uidev.id.vendor = 0x5;
+                uidev.id.product = 0x5;
+                uidev.id.version = 1;
+                uidev.absmax[uinput.ABS_X] = 255;
+                uidev.absmin[uinput.ABS_X] = 0;
+                uidev.absfuzz[uinput.ABS_X] = 0;
+                uidev.absflat[uinput.ABS_X] = 15;
+                uidev.absmax[uinput.ABS_Y] = 255;
+                uidev.absmin[uinput.ABS_Y] = 0;
+                uidev.absfuzz[uinput.ABS_Y] = 0;
+                uidev.absflat[uinput.ABS_Y] = 15;
+
+                fs.write(fd, buffer, 0, buffer.length, null, (err, written, buffer) => {
                     if (err) {
                         console.log(err);
                         throw (err);
+                    } else {
+                        ioctl(this.fd, uinput.UI_DEV_CREATE);
                     }
+                }).bind(this);
 
-                    this.fd = fd;
+            }).bind(this);
 
-                    ioctl(this.fd, uinput.UI_SET_EVBIT, uinput.EV_KEY);
-                    ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_A);
-                    ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_B);
-                    ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_X);
-                    ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_Y);
-                    ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_TL);
-                    ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_TR);
-                    ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_TL2);
-                    ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_TR2);
-                    ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_START);
-                    ioctl(this.fd, uinput.UI_SET_KEYBIT, uinput.BTN_SELECT);
-                    ioctl(this.fd, uinput.UI_SET_EVBIT, uinput.EV_ABS);
-                    ioctl(this.fd, uinput.UI_SET_ABSBIT, uinput.ABS_X);
-                    ioctl(this.fd, uinput.UI_SET_ABSBIT, uinput.ABS_Y);
-
-                    var input_id = Struct()
-                        .word16Ule('bustype')
-                        .word16Ule('vendor')
-                        .word16Ule('product')
-                        .word16Ule('version');
-
-                    var uinput_user_dev = Struct()
-                        .chars('name', uinput.UINPUT_MAX_NAME_SIZE)
-                        .struct('id', input_id)
-                        .word32Ule('ff_effects_max')
-                        .array('absmax', uinput.ABS_CNT, 'word32Sle')
-                        .array('absmin', uinput.ABS_CNT, 'word32Sle')
-                        .array('absfuzz', uinput.ABS_CNT, 'word32Sle')
-                        .array('absflat', uinput.ABS_CNT, 'word32Sle');
-
-                    uinput_user_dev.allocate();
-                    var buffer = uinput_user_dev.buffer();
-                    var uidev = uinput_user_dev.fields;
-                    buffer.fill(0);
-
-                    uidev.name = "MobileGamePad";
-                    uidev.id.bustype = uinput.BUS_USB;
-                    uidev.id.vendor = 0x5;
-                    uidev.id.product = 0x5;
-                    uidev.id.version = 1;
-                    uidev.absmax[uinput.ABS_X] = 255;
-                    uidev.absmin[uinput.ABS_X] = 0;
-                    uidev.absfuzz[uinput.ABS_X] = 0;
-                    uidev.absflat[uinput.ABS_X] = 15;
-                    uidev.absmax[uinput.ABS_Y] = 255;
-                    uidev.absmin[uinput.ABS_Y] = 0;
-                    uidev.absfuzz[uinput.ABS_Y] = 0;
-                    uidev.absflat[uinput.ABS_Y] = 15;
-
-                    fs.write(fd, buffer, 0, buffer.length, null, function(err, written, buffer) {
-                        if (err) {
-                            console.log(err);
-                            throw (err);
-                        } else {
-                            ioctl(this.fd, uinput.UI_DEV_CREATE);
-                        }
-                    }.bind(this));
-
-                }.bind(this)
-            );
-
-        }.bind(this),
+        },
 
         sendEvent: function(event) {
 
